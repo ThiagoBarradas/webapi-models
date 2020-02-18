92d75423fc77496dced36405ba76a856e4db945e
Thiago Barradas <th.barradas@gmail.com>
2020-02-18 19:24:55 -0300

    :art: include list request and response

---
+++ WebApi.Models/Request/ListRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Models.Request
+{
+    public class ListRequest
+    {
+        public int Page { get; set; } = 1;
+
+        public int Size { get; set; } = 10;
+    }
+}
---
+++ WebApi.Models/Response/ListResponse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using WebApi.Models.Request;
+
+namespace WebApi.Models.Response
+{
+    public class ListResponse<T>
+    {
+        public ListResponse()
+        {
+            this.Items = new List<T>();
+            this.Paging = new PagingResponse();
+        }
+
+        public ListResponse(List<T> items, ListRequest request, int totalItems = 0)
+        {
+            this.Items = items;
+            this.Paging = new PagingResponse(request, totalItems);
+        }
+
+        public List<T> Items { get; set; }
+
+        public PagingResponse Paging { get; set; }
+    }
+}
