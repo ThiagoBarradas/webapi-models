ea50f165e65f9c2abafabcdca4ebd5327aeb23bc
Eduardo Cabral <eduardosbcabral@gmail.com>
2021-06-18 18:18:17 -0300

    🐛 Fixes the default value for the pagination values

--- WebApi.Models/Request/ListRequest.cs
+++ WebApi.Models/Request/ListRequest.cs
@@ -10,8 +10,30 @@
             this.Size = size;
         }
 
-        public int Page { get; set; } = 1;
+        private int _page = 1;
+        public int Page
+        {
+            get => _page;
+            set
+            {
+                if(value != 0)
+                {
+                    _page = value;
+                }
+            }
+        }
 
-        public int Size { get; set; } = 10;
+        private int _size = 10;
+        public int Size
+        {
+            get => _size;
+            set
+            {
+                if (value != 0)
+                {
+                    _size = value;
+                }
+            }
+        }
     }
 }
